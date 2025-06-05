c9e8268b7f236530ee1931fe09d20bfa12a234da
jpinna99 <jpinna99@yahoo.com>
2025-06-05 15:38:08 -0400

    Initial commit with character object in place

---
+++ script.js
@@ -0,0 +1,24 @@
+const characterFeatures = {
+    race: ['Dwarf', 'Elf', 'Halfling', 'Human', 'Dragonborn', 'Gnome', 'Half-Elf', 'Half-Orc', 'Tiefling', 
+        'Aasimar', 'Goliath', 'Aarakocra', 'Astral Elf', 'Autognome', 'Bugbear', 'Centaur', 'Changeling',
+        'Deep Gnome', 'Dhampir', 'Duergar', 'Eladrin', 'Fairy', 'Firbolg', 'Genasi', 'Giff', 'Githyanki', 
+        'Githzerai', 'Goblin', 'Hadozee', 'Harengon', 'Hexblood', 'Hobgoblin', 'Kalashtar', 'Kender',
+        'Kenku', 'Kobold', 'Leonin', 'Lizardfolk', 'Loxodon', 'Minotaur', 'Owlin', 'Plasmoid', 'Reborn', 
+        'Satyr', 'Sea', 'Elf', 'Shadar-kai', 'Shifter', 'Simic Hybrid', 'Tabaxi', 'Thri-kreen', 'Tortle',
+        'Triton', 'Vedalken', 'Verdan', 'Warforged', 'Yuan-ti'],
+
+    class: ['Artificer', 'Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 
+        'Rogue', 'Sorcerer', 'Warlock', 'Wizard'],
+
+    alignment: [['Lawful', 'Neutral', 'Chaotic'], ['Good', 'Neutral', 'Evil']],
+
+    background: ['Acolyte', 'Anthropologist', 'Archaeologist', 'Athlete', 'Charlatan', 'City Watch',
+        'Clan Crafter',  'Cloistered Scholar', 'Courtier', 'Criminal', 'Entertainer', 'Faceless', 
+        'Faction Agent', 'Far Traveler', 'Feylost', 'Fisher', 'Folk Hero', 'Giant Foundling', 'Gladiator',      
+        'Guild Artisan', 'Guild Merchant', 'Haunted One', 'Hermit', 'House Agent', 'Inheritor', 'Investigator', 
+        'Knight', 'Knight of the Order', 'Marine', 'Mercenary', 'Veteran', 'Noble', 'Outlander', 'Pirate',
+        'Rewarded', 'Ruined', 'Rune Carver', 'Sage', 'Sailor', 'Shipwright', 'Smuggler', 'Soldier', 'Spy',          
+        'Urban Bounty Hunter', 'Urchin', 'Waterdhavian Noble', 'Witchlight Hand']
+}
+
+
